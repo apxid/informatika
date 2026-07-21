9eddca906edef3a0f1e832490a1ff21d19117a7d
apxid <fitra.mbcell@gmail.com>
2026-07-22 05:17:33 +0700

    Create questions.js

---
+++ icb/tebak_gambar/js/questions.js
@@ -0,0 +1,19 @@
+// Database Soal Tebak Arti Gambar
+const questionsData = [
+    {
+        image: "images/soal1.jpg",
+        answer: "disuapin ayang"
+    },
+    {
+        image: "images/soal2.jpg",
+        answer: "beli pembalut"
+    },
+    {
+        image: "images/soal3.jpg",
+        answer: "tuhan tolong aku"
+    },
+    {
+        image: "images/soal4.jpg",
+        answer: "perutmu buncit"
+    }
+];
